82f05ff37a361d256984c7a3000e69eb2bc03e34
WaiLinnAungIsHandsome <you@example.com>
2023-03-14 17:06:29 +0630

    sign in

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,37 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'sans':['Poppins']
+        },
+    },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#0067f3",
+        
+"secondary": "#fde047",
+        
+"accent": "#93c5fd",
+        
+"neutral": "#3D4451",
+        
+"base-100": "#FFFFFF",
+        
+"info": "#3ABFF8",
+        
+"success": "#36D399",
+        
+"warning": "#FBBD23",
+        
+"error": "#F87272",
+        },
+      },
+    ],
+  },
 };
